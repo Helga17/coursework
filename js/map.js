f2ecdc53dd20d7bcb3f5b8b33be49c50e5e609ad
Olya <1999olechka@gmail.com>
2018-11-17 13:45:56 +0200

    filters trees

--- js/map.js
+++ js/map.js
@@ -10,7 +10,7 @@ var trees = [
             lat: 47.839160,
             lng: 35.140104
         },
-        // "src": "http://newacropolis.org.ua/uploads/production/ckeditor/picture/data/8ed/bd5/36-/8edbd536-025a-40bb-a9fb-910f290983b1/content.png"
+        "src": "http://newacropolis.org.ua/uploads/production/ckeditor/picture/data/8ed/bd5/36-/8edbd536-025a-40bb-a9fb-910f290983b1/content.png"
     },
     {
         "_id": "234",
@@ -23,7 +23,7 @@ var trees = [
             lat: 47.848506,
             lng: 35.124971
         },
-        "src": "../images/bad.png"
+        "src": "https://glav-dacha.ru/wp-content/uploads/2017/02/grusha-pamyati-yakovleva.jpg"
     },
     {
         "_id": "345",
@@ -37,7 +37,7 @@ var trees = [
             lng: 35.122589
         },
 
-        // "src": "http://stroyres.net/wp-content/uploads/2015/08/kak-vyiglyadit-pushistyiy-yasen.jpg"
+        "src": "http://galerey-room.ru/images/065958_1385524798.png"
     },
     {
         "_id": "456",
@@ -50,7 +50,7 @@ var trees = [
             lat: 47.847178079310076,
             lng: 35.125194941190784
         },
-        //"src": "http://newacropolis.org.ua/uploads/production/ckeditor/picture/data/8ed/bd5/36-/8edbd536-025a-40bb-a9fb-910f290983b1/content.png"
+        "src": "http://galerey-room.ru/images/072420_1385526260.png"
     },
     {
         "_id": "567",
@@ -64,7 +64,7 @@ var trees = [
             lng: 35.121649300287686
         },
 
-        //"src": "http://stroyres.net/wp-content/uploads/2015/08/kak-vyiglyadit-pushistyiy-yasen.jpg"
+        "src": "http://stroyres.net/wp-content/uploads/2015/08/kak-vyiglyadit-pushistyiy-yasen.jpg"
     },
     {
         "_id": "678",
@@ -77,7 +77,7 @@ var trees = [
             lat: 47.88540829449062,
             lng: 35.02018328701047
         },
-        //"src": "http://newacropolis.org.ua/uploads/production/ckeditor/picture/data/8ed/bd5/36-/8edbd536-025a-40bb-a9fb-910f290983b1/content.png"
+        "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Kirschbaumbluete_2007.JPG/1200px-Kirschbaumbluete_2007.JPG"
     },
     {
         "_id": "789",
@@ -90,26 +90,26 @@ var trees = [
             lat: 47.84776424329802,
             lng: 35.12263195734249
         },
-        //"src": "http://newacropolis.org.ua/uploads/production/ckeditor/picture/data/8ed/bd5/36-/8edbd536-025a-40bb-a9fb-910f290983b1/content.png"
+        "src": "http://sadimsami.ru/wp-content/uploads/2017/03/vishnya-morozovka.jpg"
     },
+    
 ];
 
 var app = new Vue({
     el: '#app',
     data: {
+        //объявление переменных
         jumper: true,
+        //show: true,
         trees : [],
         map: {},
         markers: [],
-
-
-        /*объявление переменных*/
+        search : [],
         treesImages:{
             "Нормальне": "images/tree.jpg",
             "Напівсухе": "images/good-bad.png",
             "Сухе": "images/bad.png",
         },
-
         itemTrees: "",
         statureTrees: "",
         diameterTrees: "",
@@ -120,8 +120,7 @@ var app = new Vue({
         srcTrees: "images/tree.jpg",
         id: 0,
         editId: 0,
-
-        /*для редактирования*/
+        //для редактирования
         isEdit: false,
         editTreeElement:"",
         editItemTrees: "",
@@ -131,8 +130,7 @@ var app = new Vue({
         editStateTrees: "",
         editLatTrees: "",
         editLngTrees: "",
-
-        /*для просмотра информации*/
+        //для просмотра информации
         detailId: 0,
         detailItemTrees: "",
         detailStatureTrees: "",
@@ -141,11 +139,9 @@ var app = new Vue({
         detailStateTrees: "",
         detailLatTrees: "",
         detailLngTrees: ""
-
     },
-
     methods: {
-        /*добавление данных про дерево*/
+        //добавление данных про дерево
         addTree: function(){
             let position = {
                     "lat": this.latTrees,
@@ -164,7 +160,7 @@ var app = new Vue({
             this.initTreeMarker(tree);
             this.clear();
         },
-        /*считывание данных для редактирования*/
+        //считывание данных для редактирования
         editing: function(index){
             var treeEdit = this.trees.find(function(el){
                 return el._id === index;
@@ -180,15 +176,13 @@ var app = new Vue({
             this.editLngTrees = treeEdit.position.lng;
             this.isEdit = true;
         },
-
-        /*редактирование данных*/
+        //редактирование данных
         editTree: function() {
             var treeEdit = this.editTreeElement,
                 position = {
                     "lat": this.editLatTrees,
                     "lng": this.editLngTrees
                 };
-
             treeEdit.item = this.editItemTrees;
             treeEdit.stature = this.editStatureTrees;
             treeEdit.diameter = this.editDiameterTrees;
@@ -199,7 +193,7 @@ var app = new Vue({
             this.markers[this.editId].setPosition(position);
             this.clear();
         },
-        /*просмотр информации про дерево*/
+        //просмотр информации про дерево
         detail: function(index){
             let treeDetail = this.trees.find(function(el){
                 return el._id === index;
@@ -214,14 +208,11 @@ var app = new Vue({
             this.detailLatTrees = position.lat;
             this.detailLngTrees = position.lng;
 
-            //////////////////////////////
             this.jumper = false;
         },
-        //////////////////////////////
         back: function(){
             this.jumper = true;
         },
-
         //
         initTreeMarker: function (tree) {
             var self = this, url;
@@ -248,7 +239,6 @@ var app = new Vue({
         },
         //удаление
         deleteTree: function(index, arrayKey) {
-            debugger;
             this.trees.splice(arrayKey, 1);
             this.markers[index].setMap(null);
         },
@@ -270,9 +260,29 @@ var app = new Vue({
             this.editLatTrees = "";
             this.editLngTrees = "";
         }
-
     },
+    //
+    computed:{
+        filteredItems: function() {
 
+            if(!this.search.length){
+                return this.trees;
+            }
+        
+            this.markers.forEach((marker, id) => {
+                this.markers[id].setMap(null);
+            });
+        
+            var filtersTrees = this.trees.filter(element => {
+                return (app.search.includes(element.state));
+            });
+            
+            filtersTrees.forEach((tree) => {
+                this.markers[tree._id].setMap(this.map);
+            });
+        } 
+    },
+    //
     mounted: function () {
         var self = this;
         this.trees = trees;
@@ -283,7 +293,6 @@ var app = new Vue({
             },
             zoom: 13
         });
-
         this.map.addListener('click', function (event) {
             //получаем координаты
             var latLng = event.latLng;
@@ -295,115 +304,11 @@ var app = new Vue({
                 self.lngTrees = latLng.lng();
             }
         });
-
         this.trees.forEach((tree) => {
             this.initTreeMarker(tree);
         });
     }
 
 });
-
-/*объявление переменных
-var title = document.getElementById("title");
-var stature = document.getElementById("stature");
-var diameter = document.getElementById("diameter");
-var age = document.getElementById("age");
-var latitude = document.getElementById("latitude");
-var longitude = document.getElementById("longitude");
-var normalState = document.getElementById("normalState");
-var goodbadState = document.getElementById("goodbadState");
-var badState = document.getElementById("badState");*/
-
-/*сохранение формы
-function saveForm() {
-  let state;
-  console.log(normalState.checked, goodbadState.checked, badState.checked);
-  /*выбор состояния
-  if(goodbadState.checked){
-    state = "Напівсухе";
-  } else if(badState.checked){
-    state = "Сухе";
-  }else{
-    state = "Нормальне"
-  }
-
-  var newTree = {
-    item: title.value,
-    stature: stature.value,
-    diameter: diameter.value,
-    age: age.value,
-    position: {
-      lat: parseFloat(latitude.value),
-      lng: parseFloat(longitude.value)
-    },
-    state: state
-  }
-  pushArray(newTree);
-  initMap();
-  closeForm();
-  clear();
-}*/
-
-/*добавляем
-function pushArray(data) {
-  trees.push(data);
-  console.log(trees);
-}*/
-
-/*очищаем
-function clear() {
-  title.value = "";
-  stature.value = "";
-  diameter.value = "";
-  age.value = "";
-}*/
-
-/*function initMap() {
-карта
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: 47.839160,
-      lng: 35.140104
-    },
-    zoom: 13
-  });
-
-  map.addListener('click', function (event) {
-    //получаем координаты
-    let latLng = event.latLng;
-    //открыли форму
-    openForm();
-    var latitude = document.getElementById('latitude');
-    latitude.value = latLng.lat();
-    var longitude = document.getElementById('longitude');
-    longitude.value = latLng.lng();
-
-  });*/
-
-/*trees.forEach(element => {
-  var url;
-  выбор картинки по состоянию
-  switch (element.state) {
-    case "Сухе":
-      url = "images/bad.png";
-      break;
-    case "Напівсухе":
-      url = "images/good-bad.png";
-      break;
-    default:
-      url = "images/tree.jpg";
-  }*/
-
-/*маркер
-var marker = new google.maps.Marker({
-  position: element.position,
-  map: map,
-  icon: {
-    url: url,
-    scaledSize: new google.maps.Size(22, 22)
-  }
-});
-});
-}*/
 
 
